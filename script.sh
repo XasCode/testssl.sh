da0b621c4f5ce58a5a2fd7d2ffeb805ec1312939
Justin <JEStaubach@users.noreply.github.com>
2022-09-20 22:02:24 -0400

    fix: deleted line accidentally

--- script.sh
+++ script.sh
@@ -16,5 +16,6 @@ done
 for j in $FILES; do 
   rm -f /home/testssl/out.json
   HOSTNAME=$(cat $(basename $j) | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep "\"host\"" | cut -f 4 -d '"')
+  /home/testssl/testssl.sh --add-ca /home/testssl/root_ca.crt -oJ /home/testssl/out.json $HOSTNAME
   gsutil cp /home/testssl/out.json gs://${PROJECT}-results/$(basename $j)
 done
