2d8b96618c7d8400dcd40f06c04383006ababa43
Justin <JEStaubach@users.noreply.github.com>
2022-09-20 22:01:21 -0400

    fix: need to define project before using

--- script.sh
+++ script.sh
@@ -2,9 +2,9 @@
 
 cd /home/testssl
 
-gsutil cp gs://${PROJECT}-step/root_ca.cert /home/testssl/root_ca.cert
-
 PROJECT=$(gcloud compute project-info describe | grep "name: " | sed --expression='s/name: //g')
+
+gsutil cp gs://${PROJECT}-step/root_ca.crt /home/testssl/root_ca.crt
 
 gsutil cp gs://${PROJECT}-results/queue/* ./
 FILES=$(gsutil ls gs://${PROJECT}-results/queue/*)
@@ -16,6 +16,5 @@ done
 for j in $FILES; do 
   rm -f /home/testssl/out.json
   HOSTNAME=$(cat $(basename $j) | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep "\"host\"" | cut -f 4 -d '"')
-  /home/testssl/testssl.sh --add-ca /home/testssl/root_ca.cert -oJ /home/testssl/out.json $HOSTNAME
   gsutil cp /home/testssl/out.json gs://${PROJECT}-results/$(basename $j)
 done
